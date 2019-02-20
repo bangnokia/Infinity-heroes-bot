d8d245f9ae662216ab33e93180a9791ba787dc88
bangnokia <bangnokia@gmail.com>
2019-02-21 01:41:31 +0700

    fix runtime when no captcha

--- bot.py
+++ bot.py
@@ -5,24 +5,24 @@ import numpy as np
 from matplotlib import pyplot as plt
 from pprint import pprint
 import pyautogui
+import threading
+
+need_detect = False
+ax = 375
+ay = 110
+game_width = 290
+game_height = 470
+
 
 def screen_shot():
-    im = ps.grab();
-    im.save('capture.png')
-    im.show()
-
-
-def crop_region():
-    im = Image.open('capture.png')
-    region = im.crop(bbox=(362, 90, 677, 590))
-    region.save('cropped.png')
-    # region.show()
+    im1 = pyautogui.screenshot(region=(ax, ay, game_width, game_height))
+    im1.save('capture.png')
 
 
 def find_if_close(cnt1, cnt2):
     row1, row2 = cnt1.shape[0], cnt2.shape[0]
-    for i in xrange(row1):
-        for j in xrange(row2):
+    for i in range(row1):
+        for j in range(row2):
             dist = np.linalg.norm(cnt1[i] - cnt2[j])
             if abs(dist) < 50:
                 return True
@@ -31,11 +31,20 @@ def find_if_close(cnt1, cnt2):
 
 
 def process_image():
-    img = cv2.imread('cropped.png', 0)
+    global need_detect
+
+    img = cv2.imread('capture.png', 0)
     ret, thresh = cv2.threshold(img, 127, 255, 0)
     contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL, 2)
 
     LENGTH = len(contours)
+
+    if LENGTH > 50:
+        need_detect = False
+        return
+
+    need_detect = True
+
     status = np.zeros((LENGTH, 1))
 
     for i, cnt1 in enumerate(contours):
@@ -53,7 +62,7 @@ def process_image():
 
     unified = []
     maximum = int(status.max()) + 1
-    for i in xrange(maximum):
+    for i in range(maximum):
         pos = np.where(status == i)[0]
         if pos.size != 0:
             cont = np.vstack(contours[i] for i in pos)
@@ -64,17 +73,20 @@ def process_image():
     cv2.drawContours(thresh, unified, -1, 255, -1)
 
     cv2.imwrite('processed.png', thresh)
-    # img2 = Image.open('processed.png')
-    # img2.show()
 
 
 def detect_captcha():
-    im = cv2.imread('processed.png')
+    if need_detect == False:
+        return
+
+    im = cv2.imread('processed.png', 0)
     im = cv2.bitwise_not(im)
     detector = cv2.SimpleBlobDetector_create()
     keypoints = detector.detect(im)
 
     is_captcha = (len(keypoints) == 2)
+
+    pprint(len(keypoints))
 
     if is_captcha:
         pprint('Found captcha!')
@@ -85,15 +97,29 @@ def detect_captcha():
     else:
         pprint('Captcha not detected!')
 
+
 def solve_captcha(keypoint1, keypoint2):
     pprint('Solving captcha...')
-    pyautogui.moveTo(keypoint1.pt[0], keypoint1.pt[1])
-    pyautogui.dragTo(keypoint2.pt[0], keypoint2.pt[1], 1, button = 'left')
+    pyautogui.moveTo(ax + keypoint1.pt[0], ay + keypoint1.pt[1])
+    pyautogui.dragTo(ax + keypoint2.pt[0], ay + keypoint2.pt[1], 1, button = 'left')
 
-screen_shot()
 
-# crop_region()
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
 
-# process_image()
+def bot():
+    pprint('Bot is running...')
+    screen_shot()
+    process_image()
+    detect_captcha()
 
-# detect_captcha()
+bot()
+set_interval(bot, 60)
+
+
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Infinity heros bot bypass captcha
+
+## Install
+
+This requires python 3 and some packages:
+
+- pyscreenshot
+- etc...
+
+## Check list
+
+[x] Make bot working
+[] Write installing documentation
+[] Move config outside the code
+[] Learn python, lmao
